530f4af283b07cae703bf200b5d00231a662ed0e
ciro mc <156836104+Ciricundio@users.noreply.github.com>
2026-04-07 13:57:41 -0500

    fix: update githubClientId in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://writing-solver-api.onrender.com',
-  githubClientId: '',
+  githubClientId: 'Ov23liGccEN9ncCZc2G9',
   appVersion: '1.0.0',
 };
